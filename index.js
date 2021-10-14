3fc8c9b3e7f13f79bb7d9076de4e8497cb48b8ba
Scottie Thompson <59934685+srt5228@users.noreply.github.com>
2021-10-13 21:25:55 -0400

    React app added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 
@@ -7,7 +8,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // API ENDPOINTS - all will be under /api
-
+app.get('/api/coinbase/BTC', (req, res) => {
+    axios.get('https://api.coinbase.com/v2/prices/BTC-USD/buy')
+        .then(data => {
+            res.status(200).json(data)
+        })
+})
 
 
 // Any request that doesn't match anything above will be redirected to React homepage
