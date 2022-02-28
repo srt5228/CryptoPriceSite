b1707e0ae1be6f893c6e80c8bcd0b4763d8bc519
Scottie Thompson <59934685+srt5228@users.noreply.github.com>
2022-02-28 15:59:58 -0500

    Refactored React so that exchange price children share a single truth repo from dashboard - working on bestprices component then will style and be done.

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,13 +1,13 @@
 
 import '../App.css';
 import Dashboard from "./Dashboard";
-import image from '.\\Capture.JPG';
+// import image from '.\\Capture.JPG';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <img src={image} alt={"weather"} />
+        {/*<img src={image} alt={"weather"} />*/}
       <Router>
         <Route path="/" exact>
             <Dashboard/>
